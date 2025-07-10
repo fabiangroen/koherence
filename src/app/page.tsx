4a299f8a7ab16551890dc15f736982cf57959e07
Fabian Groen <fabiangroen@live.nl>
2025-07-10 04:00:11 +0200

    Add avatar component and update app layout
    Introduced a new Avatar UI component using @radix-ui/react-avatar and added it to the app header. Updated the app layout to use the Satisfy font, changed the app title and description, and improved the header with a theme toggle and avatar. Updated dependencies to include @radix-ui/react-avatar and motion.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,19 @@
 import { ThemeToggle } from "@/components/ui/themetoggle";
+import { Avatar, AvatarImage, AvatarFallback } from "@/components/ui/avatar";
+import { CircleUserRound } from "lucide-react";
 
 export default function Home() {
   return (
     <div>
-      koop mijn boek
-      <ThemeToggle className="absolute top-2 right-2"></ThemeToggle>
-    </div>
+      <div className="w-full flex items-center px-4 py-2 gap-4 bg-background border-b border-border">
+        <div className="text-2xl">Koherence</div>
+        <div className="flex-1"></div>
+        <ThemeToggle />
+        <Avatar>
+          <AvatarImage src="https://github.com/shadcn.png" />
+          <AvatarFallback>CN</AvatarFallback>
+        </Avatar>
+      </div>
+    </div >
   );
 }
--- src/components/ui/themetoggle.tsx
+++ src/components/ui/themetoggle.tsx
@@ -19,7 +19,7 @@ export function ThemeToggle({ className, ...props }: React.ComponentProps<'butto
     }
 
     return (
-        <Button onClick={toggleTheme} variant="outline" size="icon" className={className} {...props}>
+        <Button onClick={toggleTheme} variant="ghost" size="icon" className={className} {...props}>
             <Sun className="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90" />
             <Moon className="absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0" />
             <span className="sr-only">Toggle theme</span>
